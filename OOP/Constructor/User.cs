50cb58835e7402623a41362064771095fbef9ff1
Yunus Emre HAŞLAK <yunusemre.haslak@zanhagrup.com>
2023-01-15 14:19:54 +0300

    Constructor konusu eklendi.

---
+++ OOP/Constructor/User.cs
@@ -0,0 +1,20 @@
+﻿namespace ObjectOrientedProgramming.OOP.Constructor;
+
+public class User
+{
+    public string Name { get; set; }
+    
+    public string Code { get; set; }
+    
+    public User()
+    {
+        Console.WriteLine($"Yapıcı metot çalıştı.");
+    }
+
+    public User(string code, string name)
+    {
+        Code = code;
+        Name = name;
+        Console.WriteLine($"{Code}--{Name}");
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,27 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-using ObjectOrientedProgramming.OOP.Abstract;
-using ObjectOrientedProgramming.OOP.BasicPrinciples.Abstraction.Abstract;
-using ObjectOrientedProgramming.OOP.BasicPrinciples.Abstraction.Concrete;
-using ObjectOrientedProgramming.OOP.BasicPrinciples.Encapsulation;
-using ObjectOrientedProgramming.OOP.BasicPrinciples.Polymorphism.Base;
-using ObjectOrientedProgramming.OOP.BasicPrinciples.Polymorphism.SubClasses;
-using UserType = ObjectOrientedProgramming.OOP.Enums.UserType;
 
-// Person person = new Person();
-// person.Name = "Yunus Emre";
-// person.Country = "Türkiye";
-//
-// //person.PersonInfo();
-// person.PersonAge("32");
 
-List<Database> databases = new List<Database>
-{
-    new SqlDatabase(),
-    new OracleDatabase()
-};
+using ObjectOrientedProgramming.OOP.Constructor;
 
-foreach (var database in databases)
-{
-    database.DatabaseSetting();
-}
+User user = new User("1234","Y.emre");
